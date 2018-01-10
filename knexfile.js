9351b75b07c49f231a6794319c95fc6d05a9bf08
Michael Quiroz <cocomjolk@hotmail.com>
2018-01-10 12:01:41 -0600

    remove hanging comma from package.json2

--- knexfile.js
+++ knexfile.js
@@ -7,36 +7,11 @@ module.exports = {
     connection: "postgres://localhost:5432/patient"
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  production: {
+    client: 'postgres',
+    connection: process.env.DATABASE_URL
   },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+
 
 };
