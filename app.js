556823f8730252268faa1206c6c2ef29d0664d9f
Michael Quiroz <cocomjolk@hotmail.com>
2018-01-10 19:41:05 -0600

    added static assets. link to template.html works

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const PORT = process.env.PORT || 8080;
-const express    = require('express');
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('public'))
+
+
 
 //created var for routes, used in app.use
 //let patients = require('./routes/patients')
@@ -13,6 +16,10 @@ let rewards = require('./routes/rewards')
 let doctors = require('./routes/doctors')
 let videos = require('./routes/videos')
 let users = require('./routes/users')
+
+app.get('/',function(req,res){
+  res.sendFile(`${__dirname}/index.html`)
+})
 
 //incoming request from client, goint to routes in routes folder.
 //app.use('/api/patients', patients);
@@ -25,4 +32,4 @@ app.use('/api/users', users);
 app.listen(PORT);
 console.log('app.js server listening on port' + PORT);
 
-// module.exports = app;
+module.exports = app;
---
+++ public/javascript/app.js
@@ -0,0 +1,21 @@
+(function() {
+
+  'use strict'
+
+  console.log('booyah');
+  angular.module("app", [])
+    .component('recovery', {
+      controller: function () {
+
+
+
+
+      },
+
+      templateUrl: "template.html"
+      // template: `
+      //       <h1>from app.js template: html</h1>
+      //   `
+
+    })
+}());
