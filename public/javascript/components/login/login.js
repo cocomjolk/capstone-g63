0a5fe4648b63d5c349055703d0e026c8d5fcf572
Michael Quiroz <cocomjolk@hotmail.com>
2018-01-19 11:42:57 -0600

    pushing cloudinary to heroku to test. Not pushing to origin master yet

--- public/javascript/components/login/login.js
+++ public/javascript/components/login/login.js
@@ -1,10 +1,13 @@
 (function() {
   angular.module('app').component('login', {
-    controller: function($http, $state, $stateParams, $window) {
+    controller: function($http, $state, $window) {
       // console.log('this is the controller for the login view');
+      const CLOUDINARY_URL ='https://api.cloudinary.com/v1_1/hxf6ors9y';
+      const CLOUDINARY_UPLOAD_PRESET = 'jd099oi8';
+
       const vm = this
 
-      vm.stateParams = $stateParams
+      // vm.stateParams = $stateParams
 
       vm.showButtonSignIn = true;
       vm.showButtonNewAccount = true;
@@ -106,6 +109,13 @@
         vm.showButtonSignIn = true;
         vm.showButtonNewAccount = true;
         vm.goBack = false;
+
+        let file = vm.cloudinaryFile;
+        console.log(file);
+        let formData = new FormData();
+        formData.append('file', file);
+        formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET)
+
         if (vm.users.patientBox) {
           if(vm.doctor == undefined){
             vm.alert = true;
@@ -113,24 +123,39 @@
           }
           $http({
             method: 'POST',
-            url: '/api/users',
-            data: {
-              first_name: vm.users.first_name,
-              last_name: vm.users.last_name,
-              password: vm.users.password,
-              email: vm.users.email,
-              phone: vm.users.phone,
-              points: 0,
-              doctor_id: vm.doctor.id,
-              img: vm.users.img
-            }
-          }).then(function(res) {
-            // store token from POST route
-            $window.localStorage.setItem('token', res.data.token);
-            delete vm.users
-            $state.go('patient-view')
+            url: CLOUDINARY_URL,
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            data: formData
+          }).then(function(cloud_res) {
+            let pic = cloud_res.data
+            console.log(cloud_res);
+            img_url = cloud_res.data.secure_url
+            $http({
+              method: 'POST',
+              url: '/api/users',
+              data: {
+                first_name: vm.users.first_name,
+                last_name: vm.users.last_name,
+                password: vm.users.password,
+                email: vm.users.email,
+                phone: vm.users.phone,
+                points: 0,
+                doctor_id: vm.doctor.id,
+                img: img_url
+              }
+            }).then(function(res) {
+              //res containes token plus user info
+              // store token from POST route
+              $window.localStorage.setItem('token', res.data.token);
+              delete vm.users
+              $state.go('patient-view')
+            }),
+            function errorCallback(response) {}
           }),
           function errorCallback(response) {}
+
         }
         else if (vm.users.doctorBox) {
           // console.log(vm.users);
